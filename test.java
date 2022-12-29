2cf7c6d147b521ec67668b89bf0290a9c07dea9f
김세현 Alex Kim Sehyeon <97427486+ntirano@users.noreply.github.com>
2022-12-29 11:22:38 +0900

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("221229_¼­¹öÇÁ·Î±×·¥ ±¸Çö:±è¼¼Çö");
+		System.out.println("221229_ì„œë²„í”„ë¡œê·¸ë¨ êµ¬í˜„ : ì´ë¦„");
 	}
 }
